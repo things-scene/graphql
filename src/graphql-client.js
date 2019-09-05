1f367fdfdcaff41c3504d6b5ae6691c7ba64f7c6
EunHyeYun <lillyeh@hatiolab.com>
2019-09-05 16:03:11 +0900

    prettier적용 및 graphql-query period에 따라 불러오도록 작업
    prettier적용 및 graphql-query 속성의 period 값에 따라 데이터 call하도록 작업

--- src/graphql-client.js
+++ src/graphql-client.js
@@ -2,68 +2,68 @@
  * Copyright © HatioLab Inc. All rights reserved.
  */
 
-import { ApolloClient } from "apollo-client";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import { ApolloLink } from "apollo-link";
-import { HttpLink } from "apollo-link-http";
-import { onError } from "apollo-link-error";
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloLink } from 'apollo-link'
+import { HttpLink } from 'apollo-link-http'
+import { onError } from 'apollo-link-error'
 
 const defaultOptions = {
   watchQuery: {
-    fetchPolicy: "no-cache",
-    errorPolicy: "ignore"
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'ignore'
   },
   query: {
-    fetchPolicy: "no-cache", //'network-only'
-    errorPolicy: "all"
+    fetchPolicy: 'no-cache', //'network-only'
+    errorPolicy: 'all'
   },
   mutate: {
-    errorPolicy: "all"
+    errorPolicy: 'all'
   }
-};
+}
 
 const ERROR_HANDLER = ({ graphQLErrors, networkError }) => {
   if (graphQLErrors)
     graphQLErrors.map(({ message, locations, path }) => {
       document.dispatchEvent(
-        new CustomEvent("notify", {
+        new CustomEvent('notify', {
           detail: {
-            level: "error",
+            level: 'error',
             message: `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
             ex: graphQLErrors
           }
         })
-      );
-    });
+      )
+    })
 
   if (networkError) {
     switch (networkError.statusCode) {
       case 401:
         /* 401 에러가 리턴되면, 인증이 필요하다는 메시지를 dispatch 한다. 이 auth 모듈 등에서 이 메시지를 받아서 signin 프로세스를 진행할 수 있다. */
         document.dispatchEvent(
-          new CustomEvent("auth-required", {
+          new CustomEvent('auth-required', {
             bubbles: true,
             composed: true
           })
-        );
-        break;
+        )
+        break
       default:
         document.dispatchEvent(
-          new CustomEvent("notify", {
+          new CustomEvent('notify', {
             detail: {
-              level: "error",
+              level: 'error',
               message: `[Network error - ${networkError.statusCode}]: ${networkError}`,
               ex: networkError
             }
           })
-        );
+        )
     }
   }
-};
+}
 
-import COMPONENT_IMAGE from "./graphql-client.png";
+import COMPONENT_IMAGE from './graphql-client.png'
 
-import { Component, DataSource, RectPath, Shape } from "@hatiolab/things-scene";
+import { Component, DataSource, RectPath, Shape } from '@hatiolab/things-scene'
 
 const NATURE = {
   mutable: false,
@@ -71,38 +71,38 @@ const NATURE = {
   rotatable: true,
   properties: [
     {
-      type: "string",
-      label: "endpoint",
-      name: "endpoint"
+      type: 'string',
+      label: 'endpoint',
+      name: 'endpoint'
     }
   ]
-};
+}
 
 export default class GraphqlClient extends DataSource(RectPath(Shape)) {
   static get image() {
     if (!GraphqlClient._image) {
-      GraphqlClient._image = new Image();
-      GraphqlClient._image.src = COMPONENT_IMAGE;
+      GraphqlClient._image = new Image()
+      GraphqlClient._image.src = COMPONENT_IMAGE
     }
 
-    return GraphqlClient._image;
+    return GraphqlClient._image
   }
 
   ready() {
-    super.ready();
+    super.ready()
 
-    this.init();
+    this.init()
   }
 
   init() {
-    var { endpoint } = this.state;
+    var { endpoint } = this.state
 
     if (!endpoint) {
-      console.warn("endpoint not defined");
-      return;
+      console.warn('endpoint not defined')
+      return
     }
 
-    var cache = new InMemoryCache();
+    var cache = new InMemoryCache()
 
     const client = new ApolloClient({
       defaultOptions,
@@ -111,20 +111,20 @@ export default class GraphqlClient extends DataSource(RectPath(Shape)) {
         onError(ERROR_HANDLER),
         new HttpLink({
           endpoint,
-          credentials: "include"
+          credentials: 'include'
         })
       ])
-    });
+    })
 
-    this._client = client;
+    this._client = client
   }
 
   get client() {
-    return this._client;
+    return this._client
   }
 
   dispose() {
-    super.dispose();
+    super.dispose()
   }
 
   render(context) {
@@ -132,19 +132,19 @@ export default class GraphqlClient extends DataSource(RectPath(Shape)) {
      * TODO role이 publisher 인지 subscriber 인지에 따라서 구분할 수 있는 표시를 추가할 것.
      */
 
-    var { left, top, width, height } = this.bounds;
+    var { left, top, width, height } = this.bounds
 
-    context.beginPath();
-    context.drawImage(GraphqlClient.image, left, top, width, height);
+    context.beginPath()
+    context.drawImage(GraphqlClient.image, left, top, width, height)
   }
 
   onchangeData(data, before) {
-    super.onchangeData(data, before);
+    super.onchangeData(data, before)
   }
 
   get nature() {
-    return NATURE;
+    return NATURE
   }
 }
 
-Component.register("graphql-client", GraphqlClient);
+Component.register('graphql-client', GraphqlClient)
--- src/graphql-query.js
+++ src/graphql-query.js
@@ -2,10 +2,10 @@
  * Copyright © HatioLab Inc. All rights reserved.
  */
 
-import COMPONENT_IMAGE from "./graphql-query.png";
-import gql from "graphql-tag";
+import COMPONENT_IMAGE from './graphql-query.png'
+import gql from 'graphql-tag'
 
-import { Component, DataSource, RectPath, Shape } from "@hatiolab/things-scene";
+import { Component, DataSource, RectPath, Shape } from '@hatiolab/things-scene'
 
 const NATURE = {
   mutable: false,
@@ -13,37 +13,44 @@ const NATURE = {
   rotatable: true,
   properties: [
     {
-      type: "string",
-      label: "client",
-      name: "client"
+      type: 'string',
+      label: 'client',
+      name: 'client'
     },
     {
-      type: "textarea",
-      label: "query",
-      name: "query"
+      type: 'number',
+      label: 'period',
+      name: 'period',
+      placeholder: 'SECONDS'
+    },
+    {
+      type: 'textarea',
+      label: 'query',
+      name: 'query'
     }
   ]
-};
+}
 
 export default class GraphqlQuery extends DataSource(RectPath(Shape)) {
   static get image() {
     if (!GraphqlQuery._image) {
-      GraphqlQuery._image = new Image();
-      GraphqlQuery._image.src = COMPONENT_IMAGE;
+      GraphqlQuery._image = new Image()
+      GraphqlQuery._image.src = COMPONENT_IMAGE
     }
 
-    return GraphqlQuery._image;
+    return GraphqlQuery._image
   }
 
   dispose() {
-    try {
-      this._client && this._client.end(true, () => {});
-    } catch (e) {
-      console.error(e);
-    }
-    delete this._client;
+    super.dispose()
+    this._stopRepeater()
 
-    super.dispose();
+    try {
+      this._client && this._client.end(true, () => {})
+    } catch (e) {
+      console.error(e)
+    }
+    delete this._client
   }
 
   render(context) {
@@ -51,30 +58,80 @@ export default class GraphqlQuery extends DataSource(RectPath(Shape)) {
      * TODO role이 publisher 인지 subscriber 인지에 따라서 구분할 수 있는 표시를 추가할 것.
      */
 
-    var { left, top, width, height } = this.bounds;
+    var { left, top, width, height } = this.bounds
 
-    context.beginPath();
-    context.drawImage(GraphqlQuery.image, left, top, width, height);
+    context.beginPath()
+    context.drawImage(GraphqlQuery.image, left, top, width, height)
   }
 
-  async onclick() {
-    var { client, query } = this.state;
+  ready() {
+    this._initGraphqlQuery()
+  }
 
-    this.client = this.root.findById(client).client;
+  get nature() {
+    return NATURE
+  }
 
+  get period() {
+    return this.state.period * 1000
+  }
+
+  set period(period) {
+    this.setState('period', period)
+    this._initGraphqlQuery()
+  }
+
+  get repeatTimer() {
+    return this._repeatTimer
+  }
+
+  set repeatTimer(repeatTimer) {
+    this._stopRepeater()
+    this._repeatTimer = repeatTimer
+  }
+
+  _initGraphqlQuery() {
+    if (!this.app.isViewMode) return
+
+    this._stopRepeater()
+    this._startRepeater()
+  }
+
+  _stopRepeater() {
+    if (this.repeatTimer) clearTimeout(this._repeatTimer)
+    this._isStarted = false
+  }
+
+  _startRepeater() {
+    this._isStarted = true
+
+    var self = this
+
+    // requestAnimationFrame 이 호출되지 않을 때는 requestData 호출도 하지 않도록 함.
+    function _() {
+      if (!self._isStarted) {
+        return
+      }
+      self.requestData()
+
+      self._repeatTimer = setTimeout(() => {
+        requestAnimationFrame(_)
+      }, self.period)
+    }
+
+    requestAnimationFrame(_)
+  }
+  async requestData() {
+    var { client, query } = this.state
+    this.client = this.root.findById(client).client
     const response = await this.client.query({
       query: gql`
         ${query}
       `
-    });
-
-    console.log("response", response);
-    this.data = response;
-  }
-
-  get nature() {
-    return NATURE;
+    })
+    console.log('response', response)
+    this.data = response
   }
 }
 
-Component.register("graphql-query", GraphqlQuery);
+Component.register('graphql-query', GraphqlQuery)
